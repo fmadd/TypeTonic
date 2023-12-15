50363f0506c8a65c0606c51e4444987d2fb58318
fmadd <anselebedeva@edu.hse.ru>
2023-12-15 21:53:25 +0300

    theEnd

--- TypeTonic/app_server.py
+++ TypeTonic/app_server.py
@@ -6,49 +6,51 @@ import re
 
 
 def get_ip():
-    '''
+    """
     Функция возвращает текущий айпи локального сервера
     :return: Айпи адресс
     :rtype str:
-    '''
+    """
     host_name = socket.gethostname()
     return str(socket.gethostbyname(host_name))
 
 
 class app_server:
-    '''
+    """
     Класс для обращения к серверу
     :param str token: Токен текущей пользовательской сессии
-    '''
+    """
+
     token = None
 
     def valid_password(self, password):  # временно закоменчено
-        '''
-        Функция проверяет надежность пароля используя соответствующее регулярное выражение
+        """
+        Функция проверяет надежность пароля используя соответствующее
+        регулярное выражение
         :param str password:
         :return: false когда пароль неккоректный
         :rtype bool:
-        '''
+        """
         # '''
-        pattern = r'^[а-яА-Яa-zA-Z0-9]{6,}$'
+        pattern = r"^[а-яА-Яa-zA-Z0-9]{6,}$"
         if re.match(pattern, password) is None:
             return False
         # '''
         return True
 
     def log_in(self, login, password):
-        '''
-        Функция проверяет может ли пользователь залогиниться в системе с данными логином и паролем
+        """
+        Функция проверяет может ли пользователь залогиниться в системе с
+        данными логином и паролем
         :param str login:
         :param str password:
-        :rises: Exception("Слишком много попыток ввода логина"), Exception("Неверный логин или пароль")
-        '''
+        :rises: Exception("Слишком много попыток ввода логина"),
+        Exception("Неверный логин или пароль")
+        """
         result_req = {"login": login, "pass": password}
         result_json = json.dumps(result_req)
         conn = http.client.HTTPConnection(get_ip(), 2000)
-        headers = {
-            'Content-type': 'application/json'
-        }
+        headers = {"Content-type": "application/json"}
         conn.request("POST", "/log", result_json, headers=headers)
         resp = conn.getresponse()
         self.token = resp.read().decode("utf-8")
@@ -60,40 +62,35 @@ class app_server:
             raise Exception("Неверный логин или пароль")
 
     def log_out(self):
-        '''
+        """
         Функция удалает токен текущей ссесии и выходит из аккаунта пользователя
-        '''
+        """
         conn = http.client.HTTPConnection(get_ip(), 2000)
-        headers = {
-            'Authorization': f'{self.token}'
-        }
+        headers = {"Authorization": f"{self.token}"}
         conn.request("GET", "/logoff", headers=headers)
         conn.close()
 
     def del_user(self):
-        '''
+        """
         Функция удаляет текущего пользователя из базы данных пользователей
         :return:
-        '''
+        """
         conn = http.client.HTTPConnection(get_ip(), 2000)
-        headers = {
-            'Authorization': f'{self.token}'
-        }
+        headers = {"Authorization": f"{self.token}"}
         conn.request("POST", "/del_user", headers=headers)
         conn.close()
 
     def reg_user(self, login, password):
-        '''
-        Функция регистрирует пользователя с заданным логином и паролем, если таких пользователей еще нет в базе
+        """
+        Функция регистрирует пользователя с заданным логином и
+        паролем, если таких пользователей еще нет в базе
         :param str login:
         :param str password:
-        '''
+        """
         result_user = {"login": login, "pass": password}
         result_json = json.dumps(result_user)
         conn = http.client.HTTPConnection(get_ip(), 2000)
-        headers = {
-            'Content-type': 'application/json'
-        }
+        headers = {"Content-type": "application/json"}
         conn.request("POST", "/reg", result_json, headers=headers)
         resp = conn.getresponse()
         stat = resp.status
@@ -101,22 +98,25 @@ class app_server:
             raise Exception("имя занято")
 
     def send_attempt(self, login, cps=0, cpm=0, acc=0, mistakes={}):
-        '''
+        """
         Функция сохраняет попытку текущего польователя в базу данных
         :param str login:
         :param float cps:
         :param float cpm:
         :param float acc:
         :param dict mistakes:
-        '''
-        result_dict = {"login": login, "curr_data": time.time(), "cps": cps, "cpm": cpm, "acc": acc,
-                       "mistakes": mistakes}
+        """
+        result_dict = {
+            "login": login,
+            "curr_data": time.time(),
+            "cps": cps,
+            "cpm": cpm,
+            "acc": acc,
+            "mistakes": mistakes,
+        }
         result_json = json.dumps(result_dict)
         conn = http.client.HTTPConnection(get_ip(), 2000)
-        headers = {
-            'Content-type': 'application/json',
-            'Authorization': f'{self.token}'
-        }
+        headers = {"Content-type": "application/json", "Authorization": f"{self.token}"}
 
         conn.request("POST", "/attempt", result_json, headers=headers)
 
@@ -124,15 +124,13 @@ class app_server:
         conn.close()
 
     def user_log(self):
-        '''
+        """
         Функция показывает последние 20 попыток текущего пользователя
         :return: Список попыток пользователя
         :rtype: list
-        '''
+        """
         conn = http.client.HTTPConnection(get_ip(), 2000)
-        headers = {
-            'Authorization': self.token
-        }
+        headers = {"Authorization": self.token}
         conn.request("GET", "/user_log", headers=headers)
         resp = conn.getresponse()  # здесь лежат попвтки
         conn.close()
@@ -140,47 +138,41 @@ class app_server:
         return resp.read().decode()
 
     def user_dynamics(self):
-        '''
-        Функция передает изменения в характеристиках пользователя. Сравниваются среднее значения за все время и последнюю попытку
-        :return: Лист, в котором отражены соответсвующие изменения характеристик
+        """
+        Функция передает изменения в характеристиках пользователя.
+        Сравниваются среднее значения за все время и последнюю попытку
+        :return: Лист, в котором отражены соответсвующие изменения
+        характеристик
         :rtype: list
-        '''
+        """
         conn = http.client.HTTPConnection(get_ip(), 2000)
-        headers = {
-            'Authorization': f'{self.token}'
-        }
+        headers = {"Authorization": f"{self.token}"}
         conn.request("GET", "/user_dynamic", headers=headers)
         stat = conn.getresponse()
         conn.close()
         return stat.read().decode("utf-8")
 
     def get_stat_user_all(self):
-        '''
+        """
         Функция показывает статистику пользователя за все время
         :return: Лист со средними показателями за все время
         :rtype: list
-        '''
+        """
         conn = http.client.HTTPConnection(get_ip(), 2000)
-        headers = {
-            'Content-type': 'application/json',
-            'Authorization': f'{self.token}'
-        }
+        headers = {"Content-type": "application/json", "Authorization": f"{self.token}"}
         conn.request("GET", "/stat_user_all", headers=headers)
         res = conn.getresponse()
         conn.close()
         return res.read().decode("utf-8")
 
     def get_top_users_all(self):
-        '''
+        """
         Функция возвращает топ пользователей за все время
         :return: Лист характеристик топа пользователй
         :rtype: list
-        '''
+        """
         conn = http.client.HTTPConnection(get_ip(), 2000)
-        headers = {
-            'Content-type': 'application/json',
-            'Authorization': f'{self.token}'
-        }
+        headers = {"Content-type": "application/json", "Authorization": f"{self.token}"}
 
         conn.request("GET", "/top_users_all", headers=headers)
         res = conn.getresponse()
@@ -189,16 +181,14 @@ class app_server:
         return res.read().decode("utf-8")
 
     def get_user_letter(self):
-        '''
-        Функция возвращает топ букв пользователя в которых он чаще всего ошибается
+        """
+        Функция возвращает топ букв пользователя в которых он чаще
+        всего ошибается
         :return: Список букв и колво ошибок в них
         :rtype: list
-        '''
+        """
         conn = http.client.HTTPConnection(get_ip(), 2000)
-        headers = {
-            'Content-type': 'application/json',
-            'Authorization': f'{self.token}'
-        }
+        headers = {"Content-type": "application/json", "Authorization": f"{self.token}"}
 
         conn.request("GET", "/top_user_letter", headers=headers)
         res = conn.getresponse()
@@ -206,32 +196,26 @@ class app_server:
         return res.read().decode("utf-8")
 
     def get_top_letter(self):
-        '''
+        """
         Функция возвращает топ самых проблемных букв всех пользователей
         :return: Список букв
         :rtype: list
-        '''
+        """
         conn = http.client.HTTPConnection(get_ip(), 2000)
-        headers = {
-            'Content-type': 'application/json',
-            'Authorization': f'{self.token}'
-        }
+        headers = {"Content-type": "application/json", "Authorization": f"{self.token}"}
         conn.request("GET", "/top_top_letter", headers=headers)
         res = conn.getresponse()
         conn.close()
         return res.read().decode("utf-8")
 
     def get_top_users_week(self):
-        '''
+        """
         Функция возвращает топ пользователей
         :return: лист с пользователями в порядке убывания
         :rtype: list
-        '''
+        """
         conn = http.client.HTTPConnection(get_ip(), 2000)
-        headers = {
-            'Content-type': 'application/json',
-            'Authorization': f'{self.token}'
-        }
+        headers = {"Content-type": "application/json", "Authorization": f"{self.token}"}
 
         conn.request("GET", "/top_users_week", headers=headers)
         res = conn.getresponse()
--- TypeTonic/app_test.py
+++ TypeTonic/app_test.py
@@ -1,116 +1,105 @@
+import json
 import unittest
 from unittest.mock import patch, Mock
 from app_server import app_server
 
 
 class app_test(unittest.TestCase):
-
-    @patch('http.client.HTTPConnection.request')
-    @patch('http.client.HTTPConnection.getresponse')
-    @patch('http.client.HTTPConnection.close')
+    @patch("http.client.HTTPConnection.request")
+    @patch("http.client.HTTPConnection.getresponse")
+    @patch("http.client.HTTPConnection.close")
     def test_login_1(self, mock_close, mock_getresponse, mock_request):
         test_instance = app_server()
 
         mock_response = Mock()
         mock_response.status = 200
-        mock_response.read.return_value = 'test_token'.encode('utf-8')
+        mock_response.read.return_value = "test_token".encode("utf-8")
         mock_getresponse.return_value = mock_response
 
+        test_instance.log_in("test_login", "test_password")
 
-        test_instance.log_in('test_login', 'test_password')
-
-        self.assertEqual(test_instance.token, 'test_token')
+        self.assertEqual(test_instance.token, "test_token")
         mock_close.assert_called_once()
 
-    @patch('http.client.HTTPConnection.request')
-    @patch('http.client.HTTPConnection.getresponse')
-    @patch('http.client.HTTPConnection.close')
-    def test_too_many_attempts_1(self, mock_close, mock_getresponse, mock_request):
-        test_instance = app_server()
-
-        mock_response = Mock()
-        mock_response.status = 402
-        mock_getresponse.return_value = mock_response
-
-        with self.assertRaises(Exception) as context:
-            test_instance.log_in('test_login', 'test_password')
-
-        self.assertEqual(str(context.exception), "Слишком много попыток ввода логина")
-        mock_close.assert_called_once()
-
-    @patch('http.client.HTTPConnection.request')
-    @patch('http.client.HTTPConnection.getresponse')
-    @patch('http.client.HTTPConnection.close')
+    @patch("http.client.HTTPConnection.request")
+    @patch("http.client.HTTPConnection.getresponse")
+    @patch("http.client.HTTPConnection.close")
     def test_invalid_login_1(self, mock_close, mock_getresponse, mock_request):
         test_instance = app_server()
-
 
         mock_response = Mock()
         mock_response.status = 401
         mock_getresponse.return_value = mock_response
 
         with self.assertRaises(Exception) as context:
-            test_instance.log_in('test_login', 'test_password')
+            test_instance.log_in("test_login", "test_password")
 
         self.assertEqual(str(context.exception), "Неверный логин или пароль")
         mock_close.assert_called_once()
 
-    @patch('app_server.re.match')
+    @patch("app_server.re.match")
     def test_valid_password(self, mock_match):
-
         test_instance = app_server()
 
         mock_match.return_value = True
 
-        test_instance.valid_password('ValidPassword123')
+        test_instance.valid_password("ValidPassword123")
 
-        mock_match.assert_called_once_with(r'^[а-яА-Яa-zA-Z0-9]{6,}$', 'ValidPassword123')
+        mock_match.assert_called_once_with(
+            r"^[а-яА-Яa-zA-Z0-9]{6,}$", "ValidPassword123"
+        )
 
-    @patch('app_server.re.match')
+    @patch("app_server.re.match")
     def test_invalid_password(self, mock_match):
         test_instance = app_server()
 
         mock_match.return_value = None
 
-        self.assertEqual(test_instance.valid_password('invalidpassword'), False)
-        mock_match.assert_called_once_with(r'^[а-яА-Яa-zA-Z0-9]{6,}$', 'invalidpassword')
+        self.assertEqual(test_instance.valid_password("invalid"), False)
+        mock_match.assert_called_once_with(r"^[а-яА-Яa-zA-Z0-9]{6,}$", "invalid")
 
-    @patch('http.client.HTTPConnection.request')
-    @patch('http.client.HTTPConnection.close')
+    @patch("http.client.HTTPConnection.request")
+    @patch("http.client.HTTPConnection.close")
     def test_del_user(self, mock_close, mock_request):
         test_instance = app_server()
-        test_instance.token = 'test_token'
+        test_instance.token = "test_token"
 
         test_instance.del_user()
 
         mock_request.assert_called_once_with(
-            "POST", "/del_user", headers={'Authorization': 'test_token'}
+            "POST", "/del_user", headers={"Authorization": "test_token"}
         )
         mock_close.assert_called_once()
 
-    '''@patch('http.client.HTTPConnection.request')
-    def test_reg_user(self, mock_request):
-        test_instance = app_server()
+    def setUp(self):
+        self.app_server = app_server()
+        self.app_server.token = 'token'
+        self.mock_http_response = Mock()
+        self.mock_http_response.read.return_value = "[{'A':4, 'E':3, 'I':7}]".encode()
+        self.mock_http_connection = Mock()
+        self.mock_http_connection.getresponse.return_value = self.mock_http_response
 
-        # Создаем имитацию объекта HTTPResponse
-        mock_response = Mock()
-        mock_response.status = 200
-        mock_response.read.return_value = 'test_token'.encode('utf-8')
+    @patch('app_server.http.client.HTTPConnection')
+    @patch('app_server.get_ip')
+    def test_get_user_letter(self, mock_get_ip, mock_http_connection):
+        mock_get_ip.return_value = '127.0.0.1'
+        mock_http_connection.return_value = self.mock_http_connection
 
-        # Настроим возвращаемое значение mock_request для возврата mock_response
-        mock_request.return_value = mock_response
+        result = self.app_server.get_user_letter()
 
-        # Вызываем функцию
-        test_instance.reg_user('test_login', 'test_password')
-
-        # Проверяем, что HTTP-запрос был отправлен с правильными параметрами
-        mock_request.assert_called_once_with(
-            "POST", "/reg", '{"login": "test_login", "pass": "test_password"}',
-            headers={'Content-type': 'application/json'}
+        self.assertIsInstance(result, str)
+        self.assertIn("A", result)
+        mock_http_connection.assert_called_with('127.0.0.1', 2000)
+        self.mock_http_connection.request.assert_called_once_with(
+            "GET", "/top_user_letter", headers={
+                "Content-type": "application/json",
+                "Authorization": "token"
+            }
         )
-        print(test_instance.token)
-        # Проверяем, что функция log_in была вызвана с правильными параметрами
-        self.assertEqual(test_instance.token, 'test_token')'''
 
-if __name__ == '__main__':
+        self.mock_http_connection.close.assert_called_once()
+
+
+
+if __name__ == "__main__":
     unittest.main()
